fecb43574a802de11262f3fdb136749e578e801c
Timon Van Overveldt <timonvo@gmail.com>
2015-12-26 05:14:41 -0800

    Remove unnecessary CFLAGS.

--- mk/cfg/arm-unknown-linux-musleabi.mk
+++ mk/cfg/arm-unknown-linux-musleabi.mk
@@ -8,10 +8,11 @@ CFG_INSTALL_ONLY_RLIB_arm-unknown-linux-musleabi = 1
 CFG_LIB_NAME_arm-unknown-linux-musleabi=lib$(1).so
 CFG_STATIC_LIB_NAME_arm-unknown-linux-musleabi=lib$(1).a
 CFG_LIB_GLOB_arm-unknown-linux-musleabi=lib$(1)-*.so
-CFG_JEMALLOC_CFLAGS_arm-unknown-linux-musleabi := -mthumb -mcpu=cortex-a9 -march=armv7-a -Wall -Werror -ggdb3 -fPIC -D__arm__=1 -mfpu=vfp -funwind-tables -DJEMALLOC_C11ATOMICS=1 $(CFLAGS)
+CFG_JEMALLOC_CFLAGS_arm-unknown-linux-musleabi := -mthumb -march=armv7-a -Wall -g -fPIC -D__arm__=1 -mfpu=vfp -DJEMALLOC_C11ATOMICS=1 $(CFLAGS)
 
-CFG_GCCISH_CFLAGS_arm-unknown-linux-musleabi := -mthumb -mcpu=cortex-a9 -march=armv7-a -Wall -Werror -ggdb3 -fPIC -D__arm__=1 -mfpu=vfp -funwind-tables $(CFLAGS)
-CFG_GCCISH_CXXFLAGS_arm-unknown-linux-musleabi := -mthumb -mcpu=cortex-a9 -march=armv7-a -fno-rtti -ggdb3 -D__arm__=1 -mfpu=vfp -funwind-tables $(CXXFLAGS)
+#CFG_GCCISH_CFLAGS_arm-unknown-linux-musleabi := -mthumb -mcpu=cortex-a9 -march=armv7-a -Wall -Werror -ggdb3 -fPIC -D__arm__=1 -mfpu=vfp -funwind-tables $(CFLAGS)
+CFG_GCCISH_CFLAGS_arm-unknown-linux-musleabi := -mthumb -march=armv7-a -Wall -g -fPIC -D__arm__=1 -mfpu=vfp $(CFLAGS)
+CFG_GCCISH_CXXFLAGS_arm-unknown-linux-musleabi := -mthumb -march=armv7-a -Wall -g -fPIC -D__arm__=1 -mfpu=vfp -fno-rtti $(CXXFLAGS)
 CFG_GCCISH_LINK_FLAGS_arm-unknown-linux-musleabi := -g
 CFG_GCCISH_DEF_FLAG_arm-unknown-linux-musleabi :=
 CFG_LLC_FLAGS_arm-unknown-linux-musleabi :=
